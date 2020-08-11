2f7cedef338af8563af17733f04b9ee0c469a584
Okunuga Aminat <makadeaminat@gmail.com>
2020-08-11 21:06:51 +0100

    controller

---
+++ App/controller/Cohort.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: HP
+ * Date: 11-Aug-20
+ * Time: 9:04 PM
+ */
+
+namespace controller;
+
+
+class Cohort
+{
+
+}
---
+++ App/controller/Mentee.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: HP
+ * Date: 11-Aug-20
+ * Time: 9:05 PM
+ */
+
+namespace controller;
+
+
+class Mentee
+{
+
+}
