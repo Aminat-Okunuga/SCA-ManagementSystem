6f7a41cd56f9db8f4179c481ca680e5024a87dfd
Okunuga Aminat <makadeaminat@gmail.com>
2020-08-11 15:46:41 +0100

    files setup

---
+++ Entity/Mentor.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: HP
+ * Date: 10-Aug-20
+ * Time: 5:15 PM
+ */
+
+class Mentor
+{
+
+}
---
+++ view/login.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: HP
+ * Date: 10-Aug-20
+ * Time: 5:16 PM
+ */
