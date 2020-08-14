7392ccd1f6c01de10385d106186e5e51e517ade8
Okunuga Aminat <makadeaminat@gmail.com>
2020-08-14 20:06:24 +0100

    User

---
+++ admin/user/create.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: HP
+ * Date: 12-Aug-20
+ * Time: 8:16 AM
+ */
+include_once '../../autoload.php';
+include_once './process.php';
+?>
+<html>
+<head>
+    <title>Create | User</title>
+</head>
+<body>
+<h3>Create User</h3>
+<div class="container">
+    <div>
+        <form action="../user/create.php" method="post">
+            <div>
+                <label for="username">Username:</label>
+                <input type="text" name="username">
+            </div>
+            <div>
+                <label for="first_name">First Name:</label>
+                <input type="text" name="fname">
+            </div>
+            <div>
+                <label for="second_name">Second Name:</label>
+                <input type="text" name="lname">
+            </div>
+            <div>
+                <label for="password">Password:</label>
+                <input type="password" name="password">
+            </div>
+            <div>
+                <label for="email">Email:</label>
+                <input type="eamil" name="email">
+            </div>
+            <div>
+                <label for="type">Type:</label>
+                <input type="text" name="type">
+            </div>
+
+            <div>
+                <textarea name="bio" id="bio" cols="30" rows="10"></textarea>
+            </div>
+            <div>
+                <input type="submit" name="create_user" value="Create">
+            </div>
+
+        </form>
+    </div>
+</div>
+</body>
+</html>
---
+++ admin/user/process.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: HP
+ * Date: 12-Aug-20
+ * Time: 8:33 AM
+ */
+include_once '../../autoload.php';
+
+use \Library\Form as Form;
+use \Library\Validator as Validator;
+
+
+try {
+    $error = "";
+    if ($_SERVER['REQUEST_METHOD'] == "POST") {
+
+    if (!isset($_POST['create_user']) && $error == null) {
+        throw new \Exception("Invalid request format,. please try again");
+    }
+
+    $username = isset($_POST['username']) ? Form::sanitise($_POST['username']) : null;
+    $fname = isset($_POST['fname']) ? Form::sanitise($_POST['fname']) : null;
+    $lname = isset($_POST['lname']) ? Form::sanitise($_POST['lname']) : null;
+    $password = isset($_POST['password']) ? Form::sanitise($_POST['password']) : null;
+    $email = isset($_POST['email']) ? Form::sanitise($_POST['email']) : null;
+    $type = isset($_POST['type']) ? Form::sanitise($_POST['type']) : null;
+    $bio = isset($_POST['bio']) ? Form::sanitise($_POST['bio']) : null;
+
+    $fnameError = Validator::validateText('First Name', $fname, 30);
+    if ($fnameError != null) {
+        throw new Exception($fnameError);
+    }$lnameError = Validator::validateText('Last Name', $lname, 30);
+    if ($lnameError != null) {
+        throw new Exception($lnameError);
+    }$usernameError = Validator::validateText('Username', $username, 30);
+    if ($usernameError != null) {
+        throw new Exception($usernameError);
+    }$passwordError = Validator::validateText('Password', $password, 30);
+    if ($passwordError != null) {
+        throw new Exception($passwordError);
+    }$emailError = Validator::validateText('Email', $email, 30);
+    if ($emailError != null) {
+        throw new Exception($emailError);
+    }$typeError = Validator::validateText('Name', $type, 30);
+    if ($typeError != null) {
+        throw new Exception($typeError);
+    }$bioError = Validator::validateText('Name', $bio, 30);
+    if ($bioError != null) {
+        throw new Exception($bioError);
+    }
+
+    $user = new Entity\User($name, $username, $fname, $lname, $password, $email, $type, $bio);
+    $result = controller\Comment::create($user);
+
+    if ($result !== true) {
+        throw new \Exception("User creation failed");
+    }
+    $message = "User created successfully";
+    echo $message;
+
+}
+} catch (\Exception $e) {
+    echo $e->getMessage();
+    exit();
+}
+
