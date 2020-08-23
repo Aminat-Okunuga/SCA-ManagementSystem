2593af2b4c631a57eba092614599f0b086db6f28
Okunuga Aminat <makadeaminat@gmail.com>
2020-08-23 06:35:32 +0100

    frontend

--- frontend/mentor.php
+++ frontend/mentor.php
@@ -1,10 +1,10 @@
 <?php
 include_once '../autoload.php';
-$users = array();
+$mentors = array();
 $error = false;
 
 try {
-    $mentors = Controller\User::getAll();
+    $mentors = Controller\Mentor::getAll();
 } catch (\Exception $e) {
     $error = $e->getMessage();
     exit($error);
@@ -59,174 +59,46 @@ try {
       </div>
     </div>  
 
-    <div class="site-section">
+    <section class="site-section" id="blog-section">
       <div class="container">
-        <div class="row">
-          <div class="col-md-8 blog-content">
-            <!-- <p class="lead">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
-            <p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>
-
-            <blockquote><p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar.</p></blockquote>
-
-            <p>The Big Oxmox advised her not to do so, because there were thousands of bad Commas, wild Question Marks and devious Semikoli, but the Little Blind Text didn’t listen. She packed her seven versalia, put her initial into the belt and made herself on the way.</p>
-
-            <p>When she reached the first hills of the Italic Mountains, she had a last view back on the skyline of her hometown Bookmarksgrove, the headline of Alphabet Village and the subline of her own road, the Line Lane. Pityful a rethoric question ran over her cheek, then she continued her way.</p>
-
-            <p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>
-
-            <blockquote><p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar.</p></blockquote>
-
-            <p>The Big Oxmox advised her not to do so, because there were thousands of bad Commas, wild Question Marks and devious Semikoli, but the Little Blind Text didn’t listen. She packed her seven versalia, put her initial into the belt and made herself on the way.</p>
-            
-            <p>When she reached the first hills of the Italic Mountains, she had a last view back on the skyline of her hometown Bookmarksgrove, the headline of Alphabet Village and the subline of her own road, the Line Lane. Pityful a rethoric question ran over her cheek, then she continued her way.</p> -->
-
-
-            <!-- <div class="pt-5">
-              <p>Categories:  <a href="#">Design</a>, <a href="#">Events</a>  Tags: <a href="#">#html</a>, <a href="#">#trends</a></p>
-            </div> -->
-
-
-            <div>
-
-<div>
-    <table border="1">
-        <thead>
-        <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Cohort</th>
-            <th>Track</th>
-            <th>Status</th>
-            <th>Date</th>
-            <!-- <th></th> -->
-        </tr>
-        </thead>
-        <tbody>
-        <?php
-        $id = 0;
-        foreach($users as $user): ?>
-
-            <tr>
-                <td><?= ++$id;?></td>
-<!--                    <td>--><?//= $mentor['name']?><!--</td>-->
-                <td>My Name</td>
-                <td><?= $mentor['cohort_id']?></td>
-                <td><?= $mentor['track_id']?></td>
-                <td><?= $mentor['status']?></td>
-                <td><?= $mentor['date_created']?></td>
-                 </tr>
-        <?php endforeach ?>
-        </tbody>
-    </table>
-</div>
-</div>
-
-
-
-
-            <div class="pt-5">
-              <h3 class="mb-5">6 Comments</h3>
-              <ul class="comment-list">
-                <?php foreach($users as $user):?>
-                <li class="comment">
-                  <div class="vcard bio">
-                    <img src="<?php echo $user['picture'];?>" alt="Image">
-                  </div>
-                  <div class="comment-body">
-                    <h3>Jacob Smith</h3>
-                    <div class="meta">January 9, 2018 at 2:21pm</div>
-                    <p>When she reached the first hills of the Italic Mountains, she had a last view back on the skyline of her hometown Bookmarksgrove, the headline of Alphabet Village and the subline of her own road, the Line Lane. Pityful a rethoric question ran over her cheek, then she continued her way.</p>
-                    <p><a href="#" class="reply">Read More</a></p>
-                  </div>
-                </li>
-                <?php endforeach?>
-
-                <li class="comment">
-                  <div class="vcard bio">
-                    <img src="images/person_3.jpg" alt="Image">
-                  </div>
-                  <div class="comment-body">
-                    <h3>Chris Meyer</h3>
-                    <div class="meta">January 9, 2018 at 2:21pm</div>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur quidem laborum necessitatibus, ipsam impedit vitae autem, eum officia, fugiat saepe enim sapiente iste iure! Quam voluptas earum impedit necessitatibus, nihil?</p>
-                    <p><a href="#" class="reply">Read More</a></p>
-                  </div>
-
-                </li>
-
-                <li class="comment">
-                  <div class="vcard bio">
-                    <img src="images/person_1.jpg" alt="Image">
-                  </div>
-                  <div class="comment-body">
-                    <h3>Jean Doe</h3>
-                    <div class="meta">January 9, 2018 at 2:21pm</div>
-                    <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar.</p>
-                    <p><a href="#" class="reply">Read More</a></p>
-                  </div>
-                </li>
-              </ul>
-              <!-- END comment-list -->
-              
-              <!-- <div class="comment-form-wrap pt-5">
-                <h3 class="mb-5">Leave a comment</h3>
-                <form action="#" class="">
-                  <div class="form-group">
-                    <label for="name">Name *</label>
-                    <input type="text" class="form-control" id="name">
-                  </div>
-                  <div class="form-group">
-                    <label for="email">Email *</label>
-                    <input type="email" class="form-control" id="email">
-                  </div>
-                  <div class="form-group">
-                    <label for="website">Website</label>
-                    <input type="url" class="form-control" id="website">
-                  </div>
-
-                  <div class="form-group">
-                    <label for="message">Message</label>
-                    <textarea name="" id="message" cols="30" rows="10" class="form-control"></textarea>
-                  </div>
-                  <div class="form-group">
-                    <input type="submit" value="Post Comment" class="btn btn-primary btn-md text-white">
-                  </div>
-
-                </form>
-              </div> -->
-            </div>
-
+        <div class="row justify-content-center mb-5">
+          <div class="col-md-8 text-center">
+            <h2 class="text-black h1 site-section-heading text-center">Mentors</h2>
           </div>
-          <div class="col-md-4 sidebar">
-            <div class="sidebar-box">
-              <form action="#" class="search-form">
-                <div class="form-group">
-                  <span class="icon fa fa-search"></span>
-                  <input type="text" class="form-control" placeholder="Type a keyword and hit enter">
-                </div>
-              </form>
-            </div>
-            <div class="sidebar-box">
-              <!-- <div class="categories"> -->
-                <!-- <h3>Total Mentees</h3> -->
-                <!-- <li><a href="#">Creatives <span>(12)</span></a></li> -->
-                <!-- <li><a href="#">News <span>(22)</span></a></li>
-                <li><a href="#">Design <span>(37)</span></a></li>
-                <li><a href="#">HTML <span>(42)</span></a></li>
-                <li><a href="#">Web Development <span>(14)</span></a></li> -->
-              <!-- </div> -->
-            </div>
-            <!-- <div class="sidebar-box">
-              <img src="images/person_1.jpg" alt="Image" class="img-fluid mb-4 w-50 rounded-circle">
-              <h3 class="text-black">About The Author</h3>
-              <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar.</p>
-              <p><a href="#" class="btn btn-primary btn-md text-white">Read More</a></p>
-            </div>
+        </div>
 
-            <div class="sidebar-box">
-              <h3 class="text-black">Paragraph</h3>
-              <p>When she reached the first hills of the Italic Mountains, she had a last view back on the skyline of her hometown Bookmarksgrove, the headline of Alphabet Village and the subline of her own road, the Line Lane. Pityful a rethoric question ran over her cheek, then she continued her way.</p>
-            </div> -->
+        <div class="row">
+          <div class="col-md-6 col-lg-4 mb-4 mb-lg-4">
+            <div class="h-entry">
+              <a href="mentee.html"><img src="images/img_1.jpg" alt="Image" class="img-fluid"></a>
+              <h2 class="font-size-regular"><a href="mentee.html">Create Beautiful Website In Less Than An Hour</a></h2>
+              <div class="meta mb-4">Ham Brook <span class="mx-2">&bullet;</span> Jan 18, 2019<span class="mx-2">&bullet;</span> <a href="#">News</a></div>
+             <!--  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus eligendi nobis ea maiores sapiente veritatis reprehenderit suscipit quaerat rerum voluptatibus a eius.</p>
+              <p><a href="#">Continue Reading...</a></p> -->
+            </div> 
+          </div>
+          <div class="col-md-6 col-lg-4 mb-4 mb-lg-4">
+            <div class="h-entry">
+              <a href="mentee.html"><img src="images/img_2.jpg" alt="Image" class="img-fluid"></a>
+              <h2 class="font-size-regular"><a href="mentee.html">Create Beautiful Website In Less Than An Hour</a></h2>
+              <div class="meta mb-4">James Phelps <span class="mx-2">&bullet;</span> Jan 18, 2019<span class="mx-2">&bullet;</span> <a href="#">News</a></div>
+             <!--  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus eligendi nobis ea maiores sapiente veritatis reprehenderit suscipit quaerat rerum voluptatibus a eius.</p>
+              <p><a href="#">Continue Reading...</a></p> -->
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-4 mb-4 mb-lg-4">
+            <div class="h-entry">
+              <a href="mentee.html"><img src="images/img_1.jpg" alt="Image" class="img-fluid"></a>
+              <h2 class="font-size-regular"><a href="mentee.html">Create Beautiful Website In Less Than An Hour</a></h2>
+              <div class="meta mb-4">James Phelps <span class="mx-2">&bullet;</span> Jan 18, 2019<span class="mx-2">&bullet;</span> <a href="#">News</a></div>
+             <!--  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus eligendi nobis ea maiores sapiente veritatis reprehenderit suscipit quaerat rerum voluptatibus a eius.</p>
+              <p><a href="#">Continue Reading...</a></p> -->
+            </div> 
+          </div>
+          
+        </div>
+      </div>
+    </section>
           </div>
         </div>
       </div>
@@ -244,6 +116,7 @@ try {
     </a>
     
     <?php include_once 'components/footer.php';?>
+
   </div> <!-- .site-wrap -->
 
   <script src="js/jquery-3.3.1.min.js"></script>
