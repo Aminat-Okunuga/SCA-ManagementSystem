2a212b6a6c832b394288e01a071a282943382191
Okunuga Aminat <makadeaminat@gmail.com>
2020-08-24 21:49:45 +0100

    frontend

--- frontend/mentee.php
+++ frontend/mentee.php
@@ -3,7 +3,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-include_once 'autoload.php';
+include_once './autoload.php';
 $mentees = array();
 $error = false;
 
